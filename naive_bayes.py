6f0a771e31c337bacbe1efb7b0da9189f412fc61
Tommy Li <tli@waveapps.com>
2017-07-30 12:22:08 -0400

    move everything into naive_bayes.py

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,7 +1,44 @@
+import csv
+from collections import defaultdict
+from decimal import Decimal
 import sys
 
-from calculations import calculate_probablility
-from input_parser import parse_csv, tokenize
+
+def parse_csv(file_name: str):
+    """
+    Expects a csv with two rows; the first containing a category, and the second containing a sentence in that category.
+    """
+    category_to_sentence_details_map = {}
+
+    with open(file_name) as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
+            tokenized_sentence = tokenize(row[1])
+            if row[0] not in category_to_sentence_details_map:
+                category_to_sentence_details_map[row[0]] = defaultdict(int)
+
+            for word in tokenized_sentence:
+                category_to_sentence_details_map[row[0]][word] += 1
+
+    return category_to_sentence_details_map
+
+
+def tokenize(sentence: str):
+    return sentence.lower().split()
+
+
+def calculate_probablility(tokens: list, token_counts: dict, delta=0.01):
+    """
+    Calculates the add-delta probability P(tokens | details)
+    """
+    total_words_in_category = len(token_counts.keys())
+    probability = Decimal('1')
+    for token in tokens:
+        counts = token_counts.get(token, delta)
+        token_probability = Decimal(counts / total_words_in_category)
+        probability = probability * token_probability
+
+    return probability
 
 
 def main():
