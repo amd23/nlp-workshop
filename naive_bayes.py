dde82c404762926bb23ec914c887d881a264f83d
tommyzli <tommyli113@gmail.com>
2017-08-12 13:09:04 -0400

    remove non alphanumeric characters when tokenizing

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,6 +1,7 @@
 import csv
 from collections import Counter
 from decimal import Decimal
+import re
 import sys
 
 
@@ -8,6 +9,27 @@ class Classifier(object):
     def __init__(self):
         self.trained_data = {}
 
+    def tokenize(self, sentence: str):
+        cleaned_string = re.sub("[\W]", " ", sentence)
+        return cleaned_string.lower().split()
+
+    def _calculate_probablility(self, classification: str, tokens: list, delta=0.01):
+        """
+        Calculates the add-delta probability P(tokens | details)
+        """
+        if classification not in self.trained_data:
+            return 0
+
+        token_counts = self.trained_data[classification]
+        total_words_in_category = sum(val for val in token_counts.values())
+        probability = Decimal('1')
+        for token in tokens:
+            counts = token_counts.get(token, delta)
+            token_probability = Decimal(counts / total_words_in_category)
+            probability = probability * token_probability
+
+        return probability
+        
     def train(self, file_name: str):
         """
         Expects a csv with two rows; the first containing a category, and the second containing a sentence in that category.
@@ -26,26 +48,6 @@ class Classifier(object):
                     category_to_token_count_map[row[0]][word] += 1
 
         self.trained_data = category_to_token_count_map
-
-    def tokenize(self, sentence: str):
-        return sentence.lower().split()
-
-    def _calculate_probablility(self, classification: str, tokens: list, delta=0.01):
-        """
-        Calculates the add-delta probability P(tokens | details)
-        """
-        if classification not in self.trained_data:
-            return 0
-
-        token_counts = self.trained_data[classification]
-        total_words_in_category = sum(val for val in token_counts.values())
-        probability = Decimal('1')
-        for token in tokens:
-            counts = token_counts.get(token, delta)
-            token_probability = Decimal(counts / total_words_in_category)
-            probability = probability * token_probability
-
-        return probability
 
     def classify(self, raw_input: str):
         tokens = self.tokenize(raw_input)
