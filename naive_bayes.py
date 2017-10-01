7d1e4d8314fb07ab74ebbe8a557ef50da74d753b
Joseph Kahn <josephbkahn@gmail.com>
2017-10-01 13:53:05 -0400

    Make the classifier stateless and use cooler Counter syntax (#1)
    * fix for python 3
    
    * \o/
    
    * \o/
    
    * \o/
    
    * \o/
    
    * \o/
    
    * \o/
    
    * more

--- naive_bayes.py
+++ naive_bayes.py
@@ -6,27 +6,24 @@ import sys
 
 
 class Classifier(object):
-    def __init__(self):
-        self.trained_data = {}
-
     def tokenize(self, sentence: str):
         cleaned_string = re.sub("[\W]", " ", sentence)
         return cleaned_string.lower().split()
 
-    def _calculate_probablility(self, classification: str, tokens: list, delta=0.01):
+    def _calculate_probablility(self, trained_data: dict, classification: str, tokens: list, delta=0.01):
         """
         Calculates the add-delta probability P(tokens | details)
         """
-        if classification not in self.trained_data:
+        if classification not in trained_data:
             return 0
 
-        token_counts = self.trained_data[classification]
+        token_counts = trained_data[classification]
         total_words_in_category = sum(val for val in token_counts.values())
         vocab_size = len(token_counts.keys())
         probability = Decimal('1')
         for token in tokens:
-            counts = Decimal(token_counts.get(token, 0) + delta)
-            token_probability = counts / (total_words_in_category + (delta * vocab_size))
+            counts = Decimal(token_counts[token] + delta)
+            token_probability = counts / Decimal(total_words_in_category + (delta * vocab_size))
             probability = probability * token_probability
 
         return probability
@@ -39,29 +36,21 @@ class Classifier(object):
 
         with open(file_name, encoding="utf8") as csv_file:
             reader = csv.reader(csv_file)
-            for row in reader:
-                tokenized_sentence = self.tokenize(row[1])
+            for (classification, sentence) in reader:
+                counter = category_to_token_count_map.setdefault(classification, Counter())
+                counter.update(self.tokenize(sentence))
 
-                if row[0] not in category_to_token_count_map:
-                    category_to_token_count_map[row[0]] = Counter()
+        return category_to_token_count_map
 
-                for word in tokenized_sentence:
-                    category_to_token_count_map[row[0]][word] += 1
-
-        self.trained_data = category_to_token_count_map
-
-    def classify(self, raw_input: str):
+    def classify(self, trained_data: dict, raw_input: str):
         tokens = self.tokenize(raw_input)
 
-        max_probablility = float("-Inf")
-        most_likely_classification = None
-        for classification, token_counts in self.trained_data.items():
-            probability = self._calculate_probablility(classification, tokens)
-            if probability > max_probablility:
-                max_probablility = probability
-                most_likely_classification = classification
+        probability, most_likely_classification = max([
+            (self._calculate_probablility(trained_data, classification, tokens), classification)
+            for classification in trained_data.keys()
+        ] + [(float("-Inf"), None)])
 
-        if max_probablility == Decimal("0"):
+        if probability == Decimal("0"):
             raise Exception("No classification found")
 
         return most_likely_classification
@@ -70,11 +59,11 @@ class Classifier(object):
 def main():
     csv_file_path = sys.argv[1]
     classifier = Classifier()
-    classifier.train(csv_file_path)
+    trained_data = classifier.train(csv_file_path)
 
     while 1:
         new_input = input('Enter a new sentence: ')
-        print('Most likely category: {}'.format(classifier.classify(new_input)))
+        print('Most likely category: {}'.format(classifier.classify(trained_data, new_input)))
         print("")
 
 
